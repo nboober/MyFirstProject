515b8eb550c9a3006bf08090003f83b911398750
nboober <nboober@terpmail.umd.edu>
2019-06-12 10:37:24 -0400

    Initial commit

---
+++ src/MyFirstProject.java
@@ -0,0 +1,7 @@
+public class MyFirstProject {
+    public static void main(String[] arg) {
+        System.out.println("Hello, World");
+        System.out.println("Today is July 19, 2015. I am alive!");
+        System.out.println("My name is Nick.");
+    }
+}
